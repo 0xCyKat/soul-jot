0b3b3ce9f4b3366a5c92473225cddb2c85ffd3f8
cykat <ps842009@student.nitw.ac.in>
2023-03-06 23:20:15 +0530

    added express validator to validate data and did some changes in db.js and also User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -24,4 +24,6 @@ const UserSchema = new Schema({
     }
 });
 
-module.exports = mongoose.model("user", UserSchema);  
+const User = mongoose.model("user", UserSchema); 
+User.createIndexes(); 
+module.exports = User; 
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,11 +1,27 @@
 const express = require("express") 
 const User = require("../models/User") 
+const { body, validationResult } = require('express-validator') 
 const router = express.Router()
 
-router.post("/", (req,res)=>{
-    const user = User(req.body)
-    user.save(); 
-    res.send(req.body)
+router.post("/",
+body('email','Enter a valid Email').isEmail(),
+body('name','Min 3 characters required').isLength({min:3}), 
+body('password','Too short').isLength({min:5}), 
+(req,res)=>{ 
+
+    const errors = validationResult(req); 
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors:errors.array()})
+    }
+
+    User.create({
+        name: req.body.name,
+        email:req.body.email,
+        password: req.body.password,
+    }).then(user => res.json(user))
+    .catch(err => {console.log(err)
+    res.json({error:"Email Already exists"})});
+
 })
 
 module.exports = router
