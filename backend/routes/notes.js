4de38f7357a77ce91a0168d1ab3a2e203a9788c5
cykat <ps842009@student.nitw.ac.in>
2023-03-08 17:30:20 +0530

    added endpoint updateNote with id as a parameter to update an existing note

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -16,7 +16,7 @@ router.get("/fetchAllNotes", fetchUser,
         }
     })
 
-// ROUTE 2: endpoint for adding notes : Login Required
+// ROUTE 2: Endpoint for adding notes : Login Required
 router.post('/addNote', fetchUser,
     body('title', 'Enter a valid title').isLength({ min: 3 }),
     body('description', 'Enter a valid description').isLength({ min: 5 }),
@@ -39,5 +39,32 @@ router.post('/addNote', fetchUser,
             console.log(err);
             res.status(500).json({ "error": "Internal error occured"});
         }
-    })
+    }) 
+
+
+// ROUTE 3: Endpoint for updating notes 
+
+router.put('/updateNote/:id', fetchUser, async (req,res) => {
+    const {title, description} = req.body;
+    
+    // create newNote object
+
+    const newNote = {}; 
+    if(title){newNote.title = title}
+    if(description){newNote.description = description}
+
+    // find note and update it
+    let note = await Note.findById(req.params.id); 
+
+    if(!note){
+        return res.status(404).json({"error":"Note not found"}); 
+    }
+
+    if(note.user.toString() !== req.user.id){
+        return res.status(401).json({"error":"Access Denied"}); 
+    }
+
+    note = await Note.findByIdAndUpdate(req.params.id, {$set: newNote}, {new:true}); 
+    res.json(note); 
+})
 module.exports = router; 
