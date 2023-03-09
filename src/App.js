2e7a5b8a6a837a49852f155cca4b572a7113c45f
cykat <ps842009@student.nitw.ac.in>
2023-03-09 16:48:25 +0530

    Usage of context API with an example

--- src/App.js
+++ src/App.js
@@ -5,17 +5,20 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import Home from "./components/Home";
 import About from "./components/About";
+import NoteState from "./context/notes/NoteState";
 
 function App() {
   return (
     <div>
-      <Router>
-      <NavBar />
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route exact path="/about" element={<About />} />
-        </Routes>
-      </Router>
+      <NoteState>
+        <Router>
+          <NavBar />
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/about" element={<About />} />
+          </Routes>
+        </Router>
+      </NoteState>
     </div>
   );
 }
--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import noteContext from "../context/notes/NoteContext";
+import React, {useContext, useEffect} from 'react';
 
 function About() {
+  const a = useContext(noteContext); 
+  
+  useEffect(() => {
+    a.update();
+  },);
+
   return (
     <div>
-      <h1>About </h1>
+      <h1>About {a.state.name} of class {a.state.class}</h1>
     </div>
   );
 }
