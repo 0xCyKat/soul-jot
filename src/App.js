79b6d7e64f0ed3f55faaf3bbd017b022ce59c992
cykat <ps842009@student.nitw.ac.in>
2023-03-09 15:06:34 +0530

    added NavBar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import './App.css';
+import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import NavBar from "./components/NavBar";
+import Home from "./components/Home";
+import About from "./components/About";
 
 function App() {
   return (
-    <div className="App">
-      <h1>This is SoulJot first draft</h1>
+    <div>
+      <Router>
+      <NavBar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/about" element={<About />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
